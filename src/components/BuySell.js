a2b01d17f7dd748140bcfb844ec8bf065d6ecd29
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-15 17:20:58 -0600

    button component

---
+++ src/components/BuySell.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const style = {};
+
+const BuySell = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "10px",
+      }}
+    >
+      <button>Buy</button>
+      <button>Sell</button>
+    </div>
+  );
+};
+
+export default BuySell;
