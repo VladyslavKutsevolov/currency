246605ab4c50332e1970f00413a35e1407740910
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-17 10:26:23 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { calculateMedian } from "./utils/helpers";
-import Chart from "./components/Chart";
 
-const date = new Date();
-const now = date.toISOString().split("T")[0];
-date.setDate(date.getDate() - 30);
-const ago30 = date.toISOString().split("T")[0];
-
-const url = `https://api.frankfurter.app/${ago30}..${now}?amount=100&from=CAD&to=USD`;
+import Chart from "./components/Chart/index";
+import useData from "./components/hooks/useData";
+import Diff from "./components/Diff";
+import BuySell from "./components/BuySell";
 
 function App() {
-  const [ratesFor30days, setRates] = useState([]);
-  const [avg30, setAvg] = useState(0);
-  useEffect(() => {
-    const get30DaysCurrencyRate = async () => {
-      try {
-        const res = await fetch(url);
-        const { rates } = await res.json();
+  const data = useData();
 
-        let result = [];
-        let data = [];
+  const medain = useMemo(() => {
+    const ratesArr = data.length ? data.map((d) => d.rate) : [];
+    return calculateMedian(ratesArr);
+  }, [data]);
 
-        for (const key in rates) {
-          const rate = {
-            date: new Date(key),
-            rate: rates[key].USD,
-          };
+  if (!data.length) {
+    return <pre>Loading...</pre>;
+  }
 
-          result = [...result, rates[key].USD];
-          data = [...data, rate];
-        }
+  const currentRate = data.length
+    ? data.map((d) => d.rate)[data.length - 1]
+    : 0;
+  const previousRate = data.length
+    ? data.map((d) => d.rate)[data.length - 2]
+    : 0;
 
-        const median = calculateMedian(result);
-
-        setRates(data);
-      } catch (e) {
-        console.log("err", e);
-      }
-    };
-    get30DaysCurrencyRate();
-  }, []);
-
-  console.log("ratesFor30days", ratesFor30days);
   return (
     <div className="App">
-      <h1>hello</h1>
-      <Chart data={ratesFor30days} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Chart />
+        <Diff data={data} current={currentRate} previous={previousRate} />
+        <BuySell current={currentRate} median={medain} />
+      </div>
     </div>
   );
 }
