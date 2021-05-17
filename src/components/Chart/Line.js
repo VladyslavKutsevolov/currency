ecd46e8a0343918aa5e074f2349db24e8156547b
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-17 10:26:05 -0600

    added line animation

--- src/components/Chart/Line.js
+++ src/components/Chart/Line.js
@@ -1,8 +1,26 @@
-import { line, curveNatural } from "d3";
+import { useEffect, useRef, useState } from "react";
+import { line, curveNatural, transition, easeSin, select } from "d3";
 export const Line = ({ data, xScale, yScale, xValue, yValue }) => {
+  const ref = useRef();
+  const [pathLength, setPathLength] = useState(0);
+
+  useEffect(() => {
+    setPathLength(ref.current.getTotalLength());
+  }, [data]);
+
+  useEffect(() => {
+    const path = select(ref.current);
+    path
+      .attr("stroke-dashoffset", pathLength)
+      .attr("stroke-dasharray", pathLength)
+      .transition(transition().ease(easeSin).duration(2500))
+      .attr("stroke-dashoffset", 0);
+  }, [pathLength]);
+
   return (
     <g className="line">
       <path
+        ref={ref}
         fill="none"
         stroke="#0099ff"
         strokeWidth="3"
